c96f210fa6cf07c1547035ba908691393e97f2c7
Bruno Gomes <gomes.bruno.ac@gmail.com>
2022-02-26 08:21:29 -0300

    feat: Double tap hold statem data types

--- src/statem_keyboard.rs
+++ src/statem_keyboard.rs
@@ -279,19 +279,93 @@ mod double_tap_key_state_machine {
 
     #[cfg(test)]
     mod tests {
+        // TODO write tests for Double Tap module
+    }
+}
 
-        #[test]
-        fn test_mod() {
+
+mod double_tap_hold_key_state_machine { 
+    use super::*;
+    use std::time::{Instant, Duration};
+
+    enum State {
+        Waiting,
+        Released,
+        Hold,
+        DoubleTap,
+        Tap
+    }
+
+    pub struct DTHKSM {
+        state: State,
+        key_conf: DoubleTapHoldKeyConf,
+        watched_key: KeyId,
+        hold_threshold: Duration,
+        retap_threshold: Duration,
+        created: Instant,
+        released: Instant,
+    }
+
+    impl DTHKSM {
+        pub fn new(hold_threshold: Duration, retap_threshold: Duration) -> Self {
+            Self {
+                hold_threshold,
+                retap_threshold,
+                state: State::Waiting,
+                key_conf: DoubleTapHoldKeyConf::default(),
+                watched_key: KeyId::default(),
+                created: Instant::now(),
+                released: Instant::now()
+            }
+        }
+    }
+
+    impl KeyStateMachine for DTHKSM {
+        type KeyConf = DoubleTapHoldKeyConf;
+
+        fn transition<'a>(&mut self, events: &'a [Event]) -> Option<KeyActionSet> {
+            match self.state {
+                //TODO figure out how to humanize these checks (macro or inline function?)
+                State::Waiting => {
+                    // check hold expiration -> send to hold
+                    // check other key tap -> send to hold
+                    // check watched_key release -> send to released
+                },
+                State::Released => {
+                    // check retap_threshold -> send to tap
+                    // check other key press -> send to tap
+                    // check key retap -> send to double tap
+                }
+                _ => (),
+            }
+
+            match self.state {
+                State::Waiting => None,
+                State::Released => None,
+                State::Tap => Some(self.key_conf.0),
+                State::Hold => Some(self.key_conf.1),
+                State::DoubleTap => Some(self.key_conf.2),
+            }
         }
 
+        fn init_machine(&mut self, key_id: KeyId, key_conf: DoubleTapHoldKeyConf) {
+            self.watched_key = key_id;
+            self.key_conf = key_conf;
+            self.created = Instant::now();
+        }
+    }
+
+    #[cfg(test)]
+    mod tests {
+
+        #[test]
+        fn test() {
+        }
     }
 }
 
 
 /*
-   mod DoubleTapHoldStateM { }
-
-
    pub struct SMKeyboard {
    num_keys,
    output_actions_buff,
