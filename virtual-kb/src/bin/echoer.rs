21b36cbcef107de45c72466fe9dfa352cc599c8e
Bruno Gomes <gomes.bruno.ac@gmail.com>
2022-04-30 23:24:12 -0300

    fix: [virtual-kb] Open device file as non-blocking in echoer

--- virtual-kb/src/bin/echoer.rs
+++ virtual-kb/src/bin/echoer.rs
@@ -1,5 +1,7 @@
 use std::time::Duration;
 use std::fs;
+use std::ffi::CString;
+use std::os::unix::io::FromRawFd;
 
 use kb_core::keyboard::Action;
 use kb_core::keyboard::Event;
@@ -7,6 +9,7 @@ use kb_core::keyboard::Keyboard;
 use kb_core::keyboard::echoer::EchoerKb;
 use clap::Arg;
 use clap::App;
+use libc;
 
 use virtual_kb::Error;
 use virtual_kb::Runtime;
@@ -23,11 +26,7 @@ fn main() {
         .get_matches();
 
     let ev_file = matches.value_of("event source").unwrap();
-    let ev_file = fs::OpenOptions::new()
-        .read(true)
-        .write(false)
-        .open(ev_file)
-        .unwrap();
+    let ev_file = open_dev(&ev_file);
 
     let event_iter = EventIter::new(ev_file).unwrap();
 
@@ -37,4 +36,13 @@ fn main() {
 
     let mut runtime = Runtime::new(event_iter, virtual_dev, keyboard, Duration::from_millis(100)).unwrap();
     runtime.run()
+}
+
+fn open_dev(path: &str) -> fs::File {
+    unsafe {
+        let flags = libc::O_NONBLOCK | libc::O_RDONLY;
+        let path = CString::new(path).unwrap();
+        let fd = libc::open(path.as_ptr(), flags);
+        fs::File::from_raw_fd(fd)
+    }
 }
