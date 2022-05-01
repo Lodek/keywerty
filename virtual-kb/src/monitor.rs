a7ea84e507488022e8d972a36a5626b8336e71de
Bruno Gomes <gomes.bruno.ac@gmail.com>
2022-04-30 23:21:34 -0300

    refactor: [virtual-kb] Monitor update to queue events internally

--- virtual-kb/src/monitor.rs
+++ virtual-kb/src/monitor.rs
@@ -10,14 +10,13 @@ use evdev_rs::enums::EventCode;
 use evdev_rs::enums::EV_KEY;
 use kb_core::keyboard::Event;
 
+
 /// Iterator that returns an Evdev event for a give device file.
 /// Calling `next` will perform a device read, which in turn will
 /// return an event.
-/// 
-/// On its own, `next` will be a blocking call, as such this should be
-/// paired with `Epoll` to build a non_blocking event loop
 pub struct EventIter {
-    device: Device
+    device: Device,
+    events: Vec<Event<EV_KEY>>
 }
 
 impl AsRawFd for EventIter {
@@ -26,38 +25,77 @@ impl AsRawFd for EventIter {
     }
 }
 
+// TODO use log facade for debug log
+// https://rust-lang-nursery.github.io/rust-cookbook/development_tools/debugging/config_log.html
+// https://docs.rs/log/latest/log/
 impl EventIter {
-    // Should I take a Device or a File?
+
     pub fn new(file: File) -> io::Result<Self> {
         let device = Device::new_from_file(file)?;
         Ok(Self {
-           device
+           device,
+           events: Vec::new()
        })
+    }
+
+    fn read_all_events(&mut self) {
+        // FIXME this implementation completely ignores the SYN_DROPPED
+        // events from evdev and must be revisted.
+        // See:
+        // - https://www.freedesktop.org/software/libevdev/doc/latest/syn_dropped.html
+        // - https://docs.rs/evdev-rs/latest/evdev_rs/struct.Device.html#method.next_event
+        loop {
+            match self.device.next_event(ReadFlag::NORMAL) {
+                Ok((_, input_event)) => {
+                    eprintln!("read event: {:?}", input_event);
+                    if let Some(event) = self.map_event(input_event) {
+                        self.events.push(event);
+                    }
+                },
+                Err(error) => {
+                    eprintln!("error reading event device: {:?}", error);
+                    return;
+                }
+            }
+        }
+    }
+
+    fn map_event(&mut self, input_event: InputEvent) -> Option<Event<EV_KEY>> {
+        match &input_event {
+            InputEvent { event_code: EventCode::EV_KEY(ev_key), value: 0, .. } => Some(Event::KeyRelease(*ev_key)),
+            InputEvent { event_code: EventCode::EV_KEY(ev_key), value: 1, .. } => Some(Event::KeyPress(*ev_key)),
+            ev => {
+                eprintln!("dropped input event: {:?}", input_event);
+                None
+            }
+        }
     }
 }
 
 impl Iterator for EventIter {
     type Item = Event<EV_KEY>;
 
+    /// Return the next event from the event queue.
+    /// If the internal queue is empty, perform a device read.
+    ///
+    /// Note that reading the device will not block, therefore it should be paired
+    /// epoll to avoid busy looping.
     fn next(&mut self) -> Option<Self::Item> {
-        // FIXME this implementation completely ignores the SYN_DROPPED
-        // events from evdev and must be revisted.
-        // See:
-        // - https://www.freedesktop.org/software/libevdev/doc/latest/syn_dropped.html
-        // - https://docs.rs/evdev-rs/latest/evdev_rs/struct.Device.html#method.next_event
-        match self.device.next_event(ReadFlag::NORMAL) {
-            Ok((_, InputEvent { event_code: EventCode::EV_KEY(ev_key), value: 0, ..})) => Some(Event::KeyRelease(ev_key)),
-            Ok((_, InputEvent { event_code: EventCode::EV_KEY(ev_key), value: 1, ..})) => Some(Event::KeyPress(ev_key)),
-            Ok(ok) => {
-                // TODO debug log with skipped value
-                // https://rust-lang-nursery.github.io/rust-cookbook/development_tools/debugging/config_log.html
-                // https://docs.rs/log/latest/log/
-                None
-            },
-            Err(err) => {
-                // TODO err log with err info
-                None
-            },
+        if self.events.is_empty() {
+            self.read_all_events();
         }
+        self.events.pop()
     }
+}
+
+#[cfg(test)]
+mod tests {
+
+    #[test]
+    fn test_iterator_return_elements() {
+        // TODO research how to mock `Device`
+        // or crate trait and implement for `Device`
+        // such that I can use a stub interface.
+    }
+
 }
