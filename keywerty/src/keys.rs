aa7c071e9c93c45d0519f28a64e0af0afa37327e
Bruno Gomes <gomes.bruno.ac@gmail.com>
2022-05-24 22:52:56 -0300

    chore: [keywerty] Docstrings for keyconf types

--- keywerty/src/keys.rs
+++ keywerty/src/keys.rs
@@ -101,6 +101,9 @@ impl<T> Default for KeyConf<T> {
     }
 }
 
+/// TapKeyConf represents a key as most people are used to.
+/// Once it's pressed (key down) it performs an action.
+/// Upon being released it undo / stop performing the action.
 #[derive(Clone, Copy, Debug)]
 pub struct TapKeyConf<T> {
     pub tap: KeyActionSet<T>,
@@ -115,6 +118,12 @@ impl<T> Default for TapKeyConf<T> {
 }
 
 
+/// `HoldKeyConf` is a stateful key configuration
+/// where the Key performs different actions when either
+/// held or tapped.
+/// In this configuration, the Hold behavior is only fired
+/// after some other key is pressed or after a predetermined
+/// time interval passes by.
 #[derive(Clone, Copy, Debug)]
 pub struct HoldKeyConf<T> {
     pub tap: KeyActionSet<T>,
@@ -131,6 +140,31 @@ impl<T> Default for HoldKeyConf<T> {
 }
 
 
+/// `EagerHoldKeyConf` is much like `HoldKeyConf`.
+/// The difference between these configurations is that
+/// the eager version will perform the `hold` action as 
+/// soon as the key is tapped / activated.
+/// If the key is released before the hold activation timer,
+/// the performed action will be undone (through `KeyActionSet::invert`),
+/// and the tap action will be executed.
+#[derive(Clone, Copy, Debug)]
+pub struct EagerHoldKeyConf<T> {
+    pub tap: KeyActionSet<T>,
+    pub hold: KeyActionSet<T>,
+}
+
+impl<T> Default for EagerHoldKeyConf<T> {
+    fn default() -> Self {
+        Self {
+            tap: KeyActionSet::default(),
+            hold: KeyActionSet::default()
+        }
+    }
+}
+
+
+/// `DoubleTapKeyConf` is a composed key configuration where the key performs
+/// one action if pressed and another if pressed, released and quickly tapped in sucession.
 #[derive(Clone, Copy, Debug)]
 pub struct DoubleTapKeyConf<T> {
     pub tap: KeyActionSet<T>,
@@ -147,6 +181,13 @@ impl<T> Default for DoubleTapKeyConf<T> {
 }
 
 
+/// `DoubleTapHoldKeyConf` merges the behavior of `HoldKeyConf` and `DoubleTapKeyConf`.
+/// The `hold` action is sent if the key is pressed and held for a specified threshold.
+/// Upon key release, if the key is retapped before the retap threshold, it will perform the
+/// `double_tap` action, otherwise it will perform the `tap` action.
+///
+/// This key configuration is often used to map the Caps Lock key into Ctrl for `hold`,
+/// ESC for `tap` and Caps Lock for `double_tap`
 #[derive(Clone, Copy, Debug)]
 pub struct DoubleTapHoldKeyConf<T> {
     pub tap: KeyActionSet<T>,
