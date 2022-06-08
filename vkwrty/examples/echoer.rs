ff8aa124c6c2d652915cd5a5142b8253eaeb3128
Bruno Gomes <gomes.bruno.ac@gmail.com>
2022-06-07 23:10:36 -0300

    refactor(vkwrty): Move echoer to examples

--- vkwrty/examples/echoer.rs
+++ vkwrty/examples/echoer.rs
@@ -1,20 +1,37 @@
+//! Sample program demosntrating vkwrty runtime usage and functionality
+//! Employs a simple "echoer" keyboard, which echoes the input as the output.
+//!
+//! This keyboard model works in Linux because the intercepted uinput event
+//! will be the same as the emitted one.
+
 use std::time::Duration;
-use std::fs;
-use std::ffi::CString;
-use std::os::unix::io::FromRawFd;
 
 use keywerty::keyboard::Action;
 use keywerty::keyboard::Event;
 use keywerty::keyboard::Keyboard;
-use keywerty::keyboard::EchoerKb;
 use clap::Arg;
 use clap::App;
-use libc;
 
 use vkwrty::Error;
 use vkwrty::Runtime;
 use vkwrty::virtual_dev::UInputKeyboard;
 use vkwrty::monitor::EventIter;
+use vkwrty::open_dev;
+
+
+/// Implementation of Keyboard trait that echoes the input event data as an action.
+pub struct EchoerKb { }
+
+impl<T> Keyboard<T, T> for EchoerKb {
+    fn transition(&mut self, event: Event<T>) -> Vec<Action<T>> {
+        match event {
+            Event::KeyPress(code) => vec![Action::SendCode(code)],
+            Event::KeyRelease(code) => vec![Action::Stop(code)],
+            Event::Poll => Vec::new()
+        }
+    }
+}
+
 
 fn main() {
     let matches = App::new("Virtual echoer Keyboard")
@@ -36,13 +53,4 @@ fn main() {
 
     let mut runtime = Runtime::new(event_iter, virtual_dev, keyboard, Duration::from_secs(300)).unwrap();
     runtime.run()
-}
-
-fn open_dev(path: &str) -> fs::File {
-    unsafe {
-        let flags = libc::O_NONBLOCK | libc::O_RDONLY;
-        let path = CString::new(path).unwrap();
-        let fd = libc::open(path.as_ptr(), flags);
-        fs::File::from_raw_fd(fd)
-    }
 }
